43e5046ee758d95b42bae7700628311322e1aa03
Judalm Park (dami) <wnekfa1004@naver.com>
2023-07-29 23:27:15 +0900

    Book TYPE Enum Class 활용

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -1,16 +1,14 @@
 package com.group.libraryapp.domain.book
 
 import java.lang.IllegalArgumentException
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 class Book (
         val name: String,
 
-        val type: String,
+        @Enumerated(EnumType.STRING)
+        val type: BookType,
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,9 +23,9 @@ class Book (
 
     companion object {
         fun fixture(
-                name: String = "책 이름",
-                type: String = "COMPUTER",
-                id: Long? = null,
+            name: String = "책 이름",
+            type: BookType = BookType.COMPUTER,
+            id: Long? = null,
         ): Book {
             return Book(
                 name = name,
